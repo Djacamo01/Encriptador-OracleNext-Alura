c0f22a051ffa8e2341b7e75cdb5571f7cd1aaae8
Djacamo01 <DONALD.JACAMO22@est.unanleon.edu.ni>
2023-08-09 13:50:48 -0600

    proyecto encriptador finalizado

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
+// Funciones de encriptación y desencriptación
+
+function encriptar(mensaje) {
+    return mensaje
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+}
+
+function desencriptar(mensajeEncriptado) {
+    return mensajeEncriptado
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+}
+
+// Manejo del DOM
+
+document.addEventListener("DOMContentLoaded", () => {
+    const areaTexto = document.getElementById("areaTexto");
+    const areaTextoEncriptado = document.getElementById("areaTextoEncriptado");
     const botonEncriptar = document.getElementById("botonEncriptar");
     const botonDesencriptar = document.getElementById("botonDesencriptar");
     const botonCopiar = document.getElementById("botonCopiar");
-    const areaTexto = document.getElementById("areaTexto");
-    const areaTextoEncriptado = document.getElementById("areaTextoEncriptado");
 
-    botonEncriptar.addEventListener("click", function () {
-        const mensaje = areaTexto.value;
+    botonEncriptar.addEventListener("click", () => {
+        const mensaje = areaTexto.value.toLowerCase();
         const mensajeEncriptado = encriptar(mensaje);
         areaTextoEncriptado.value = mensajeEncriptado;
     });
 
-    botonDesencriptar.addEventListener("click", function () {
-        const mensajeEncriptado = areaTextoEncriptado.value;
-        const mensaje = desencriptar(mensajeEncriptado);
-        areaTexto.value = mensaje;
+    botonDesencriptar.addEventListener("click", () => {
+        const mensaje = areaTexto.value.toLowerCase();
+        const mensajeDesencriptado = desencriptar(mensaje);
+        areaTextoEncriptado.value = mensajeDesencriptado;
     });
 
-    botonCopiar.addEventListener("click", function () {
+    botonCopiar.addEventListener("click", () => {
         areaTextoEncriptado.select();
         document.execCommand("copy");
     });
-
-    function encriptar(texto) {
-        const mapaEncriptacion = {
-            "e": "enter",
-            "i": "imes",
-            "a": "ai",
-            "o": "ober",
-            "u": "ufat"
-        };
-
-        return texto.split("").map(char => mapaEncriptacion[char] || char).join("");
-    }
-
-    function desencriptar(textoEncriptado) {
-        const mapaDesencriptacion = {
-            "enter": "e",
-            "imes": "i",
-            "ai": "a",
-            "ober": "o",
-            "ufat": "u"
-        };
-
-        return textoEncriptado.split(" ").map(word => mapaDesencriptacion[word] || word).join("");
-    }
 });
